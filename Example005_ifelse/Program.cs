a407e9905c47a6311d75bbce1e76dfaca0247120
AndreiBrilliantov <andrewbrull@gmail.com>
2023-06-08 17:15:24 +0300

    init Example005

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "masha")
+{
+   Console.WriteLine("Ура! Это "+userName); 
+} else
+{
+    Console.WriteLine("Это не Masha");
+}
