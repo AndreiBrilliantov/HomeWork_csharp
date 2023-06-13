ba5a0378d15925956474fa651f2365365955e345
AndreiBrilliantov <andrewbrull@gmail.com>
2023-06-13 17:23:46 +0300

    add Example 008 - 011

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,9 @@
+﻿int Max(int arg1, int arg2, int arg3) {
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+    }
+int a1 = 12, a2 = 32, a3 = 65;
+int max = Max(a1, a2, a3);
+Console.WriteLine(max);
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 12, 13, 23, 35, 12, 1};
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
