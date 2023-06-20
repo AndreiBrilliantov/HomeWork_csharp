4d404c55ee94cde83baab573e9d3357fc226c7c1
AndreiBrilliantov <andrewbrull@gmail.com>
2023-06-20 16:14:49 +0300

    add Example012_functions

---
+++ Example012_functions/Program.cs
@@ -0,0 +1,22 @@
+﻿// void - функции ничего не возвращают
+void Method2(string msg) {
+    Console.WriteLine(msg);
+}
+//Явное указание передаваемых аргументов
+Method2(msg: "Text");
+
+
+// Метод который возвращает значения
+String Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; //Задает пустую строку
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(text: "Привет", count: 4);
+Console.WriteLine(res);
